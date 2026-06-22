aa8828a1b92bd31d0db03fde6660f5c09dcd9114
Benedikt <bs444@email.uni-freiburg.de>
2026-06-22 19:07:10 +0200

    gitignore

--- src/terror_and_fdi/data_cleaning/cleaning_raw_sources.py
+++ src/terror_and_fdi/data_cleaning/cleaning_raw_sources.py
@@ -3,7 +3,12 @@ Loads and cleans all sources for the main analysis.
 Always configure input and output path as well as needed variables.
 """
 
+
+# !!! CHECK FOR ALL SOURCES WHETHER ISO3 IS AVAILABLE !!!
+
 import pandas as pd
+import country_converter as coco
+import logging
 from terror_and_fdi.config import RAW, INTERIM
 
 # ==============================================================
@@ -12,8 +17,14 @@ from terror_and_fdi.config import RAW, INTERIM
 # Specify which sources need to be cleaned
 CLEAN_V_DEM = False
 CLEAN_PWT = False
+CLEAN_TAX = True
+CLEAN_UCDP = True
+CLEAN_WORLD_BANK = True
+
 CLEAN_GTD = False
 
+logging.getLogger('country_converter').setLevel(logging.CRITICAL)
+cc = coco.CountryConverter()
 
 # ==============================================================
 # V-DEM
@@ -84,9 +95,12 @@ if CLEAN_V_DEM:
          "scale": "0-4"
         },
     ]
+
     V_DEM_VARS = [item["code"] for item in V_DEM_VARS_DICT]
 
     v_dem_df = pd.read_csv(V_DEM_INPUT, usecols = V_DEM_VARS)
+
+    v_dem_df = v_dem_df.drop(columns = ["country_name"]).rename(columns = {"country_text_id": "ISO3"})
 
     v_dem_df.to_csv(V_DEM_OUTPUT, index = False)
 
@@ -106,16 +120,46 @@ if CLEAN_PWT:
 # ==============================================================
 # TAX FOUNDATION
 # ==============================================================
+if CLEAN_TAX:
+    TAX_INPUT = RAW / "tax_foundation" / "rates_final.csv"
+    TAX_OUTPUT = INTERIM / "stat_tax_rate_processed.csv"
 
-TAX_INPUT = RAW / "tax_foundation" / "rates_final.csv"
-TAX_OUTPUT = INTERIM / "stat_tax_rate_processed.csv"
+    tax_df = pd.read_csv(TAX_INPUT)
 
-tax_df = pd.read_csv(TAX_INPUT)
+    tax_df = tax_df.drop(columns = ["iso_2", "continent", "country"])
+    tax_df = pd.melt(
+        tax_df, id_vars="iso_3", var_name = "year", value_name="stat_corp_tax_rate"
+    )
 
-tax_df = tax_df.drop(columns = ["iso_2", "continent", "country"])
-tax_df = pd.melt(
-    tax_df, id_vars="iso_3", var_name = "year", value_name="stat_corp_tax_rate"
-)
-tax_df = tax_df.sort_values(by = "iso_3")
-tax_df.to_csv(TAX_OUTPUT, index = False)
-print(tax_df)
+    tax_df = tax_df.sort_values(by = "iso_3").rename(columns = {"iso_3": "ISO3"})
+    tax_df.to_csv(TAX_OUTPUT, index = False)
+
+
+# ==============================================================
+# UCDP
+# ==============================================================
+if CLEAN_UCDP:
+    UCDP_INPUT = RAW / "ucdpprio" / "OrganizedViolenceCYDataSet26_1.csv"
+    UCDP_OUTPUT = INTERIM / "state_based_violence_processed.csv"
+    UCDP_VARS = ["country", "year", "sb_exist", "sb_intrastate_exist", "sb_interstate_exist"]
+
+    df = pd.read_csv(UCDP_INPUT, usecols = UCDP_VARS)
+    df = df[~df["country"].str.contains("German Democratic Republic")]
+    df["ISO3"] = cc.convert(names = df["country"], to = "ISO3")
+    df = df.drop(columns = "country")
+
+    df.to_csv(UCDP_OUTPUT, index = False)
+
+
+# ==============================================================
+# WORLD BANK
+# ==============================================================
+if CLEAN_WORLD_BANK:
+    WORLD_BANK_INPUT = RAW / "world_bank" / "wb_data.csv"
+    WORLD_BANK_OUTPUT = INTERIM / "wb_data_processed.csv"
+
+    df = pd.read_csv(WORLD_BANK_INPUT)
+    df["ISO3"] = cc.convert(names = df["country_code"], to = "ISO3")
+    df = df.drop(columns= ["country", "country_code"])
+
+    df.to_csv(WORLD_BANK_OUTPUT, index = False)
